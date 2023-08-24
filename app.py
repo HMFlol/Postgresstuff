f55416c72d37811b9f7eb95ef19ad0443021fbe6
HMFlol <holy.mnemonic@gmail.com>
2023-08-24 09:10:47 -0300

    Adding retry

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 import psycopg2
 import os
+import time
 
 host = os.environ['PG_HOST']
 user = os.environ['PG_USER']
 password = os.environ['PG_PASSWORD']
 database = os.environ['PG_DB']
 
-try:
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        dbname=database
-    )
-    print("Connected to PostgreSQL")
-except Exception as e:
-    print("Error:", e)
-finally:
-    if connection:
-        connection.close()
-        print("Connection closed")
+# Retry connection for a certain number of times
+max_retries = 10
+retry_interval = 5
+
+for retry in range(max_retries):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            dbname=database
+        )
+        print("Connected to PostgreSQL")
+        break
+    except Exception as e:
+        print(f"Attempt {retry + 1}/{max_retries}: Error connecting to database - {e}")
+        time.sleep(retry_interval)
+else:
+    print("Max retries reached. Could not connect to the database.")
+
+# Close the connection
+if connection:
+    connection.close()
+    print("Connection closed")
+
